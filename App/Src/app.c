4348ab6ab89e44f5fdffa299c66a9a576a1710ec
okapy142 <okapy142@gmail.com>
2016-08-26 16:29:24 +0900

    コードを整形

--- App/Src/app.c
+++ App/Src/app.c
@@ -11,10 +11,11 @@
 
 static
 int suspensionSystem(void);
+
 /*メモ
  * g_ab_h...ABのハンドラ
  * g_md_h...MDのハンドラ
- * 
+ *
  * g_rc_data...RCのデータ
  */
 
@@ -36,36 +37,37 @@ int appInit(void){
 /*application tasks*/
 int appTask(void){
   int ret = 0;
-  if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_R2(g_rc_data)&&
-     __RC_ISPRESSED_L1(g_rc_data)&&__RC_ISPRESSED_L2(g_rc_data)){
-    while(__RC_ISPRESSED_R1(g_rc_data)||__RC_ISPRESSED_R2(g_rc_data)||
-	  __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
+  if( __RC_ISPRESSED_R1(g_rc_data) && __RC_ISPRESSED_R2(g_rc_data) &&
+      __RC_ISPRESSED_L1(g_rc_data) && __RC_ISPRESSED_L2(g_rc_data)){
+    while( __RC_ISPRESSED_R1(g_rc_data) || __RC_ISPRESSED_R2(g_rc_data) ||
+           __RC_ISPRESSED_L1(g_rc_data) || __RC_ISPRESSED_L2(g_rc_data)){
+    }
     ad_main();
   }
-  
+
   /*それぞれの機構ごとに処理をする*/
   /*途中必ず定数回で終了すること。*/
   ret = suspensionSystem();
   if( ret ){
     return ret;
   }
-  
+
   ret = SteerCtrl();
   if( ret ){
     return ret;
   }
-  
+
   ret = ArmRotate();
   if( ret ){
     return ret;
   }
-  
+
   ret = ArmSystem();
   if( ret ){
     return ret;
   }
   return EXIT_SUCCESS;
-}
+} /* appTask */
 
 /*Private ステア制御*/
 static
@@ -82,17 +84,17 @@ int SteerCtrl(void){
 static
 int ArmSystem(void){
   static int prs_tri_s = 0;
-  if ( (__RC_ISPRESSED_L1(g_rc_data)) &&
-       (__RC_ISPRESSED_R1(g_rc_data)) &&
-       (__RC_ISPRESSED_TRIANGLE(g_rc_data)) ) {
-    if (prs_tri_s == 0) {
+  if(( __RC_ISPRESSED_L1(g_rc_data)) &&
+     ( __RC_ISPRESSED_R1(g_rc_data)) &&
+     ( __RC_ISPRESSED_TRIANGLE(g_rc_data))){
+    if( prs_tri_s == 0 ){
       g_ab_h[DRIVER_AB].dat ^= ARM_OC_AB;
       g_ab_h[DRIVER_VM].dat ^= STICK_BOX_VM;
       prs_tri_s = 1;
     }
   } else {
     prs_tri_s = 0;
-  } 
+  }
   return EXIT_SUCCESS;
 }
 
@@ -100,21 +102,21 @@ int ArmSystem(void){
 static
 int ArmRotate(void){
   const int fakean = 8;
-  if ( (__RC_ISPRESSED_UP(g_rc_data)) &&
-      !(__RC_ISPRESSED_DOWN(g_rc_data)) &&
-       (MW_GPIORead(GPIOBID, GPIO_PIN_15) == 0) ){
+  if(( __RC_ISPRESSED_UP(g_rc_data)) &&
+     !( __RC_ISPRESSED_DOWN(g_rc_data)) &&
+     ( MW_GPIORead(GPIOBID, GPIO_PIN_15) == 0 )){
     g_md_h[ARM_MOVE_MD].mode = D_MMOD_FORWARD;
     g_md_h[ARM_MOVE_MD].duty = fakean * MD_GAIN;
     return EXIT_SUCCESS;
   }
-  if ( (__RC_ISPRESSED_DOWN(g_rc_data)) &&
-      !(__RC_ISPRESSED_UP(g_rc_data)) &&
-       (MW_GPIORead(GPIOCID, GPIO_PIN_0) == 0) ){
+  if(( __RC_ISPRESSED_DOWN(g_rc_data)) &&
+     !( __RC_ISPRESSED_UP(g_rc_data)) &&
+     ( MW_GPIORead(GPIOCID, GPIO_PIN_0) == 0 )){
     g_md_h[ARM_MOVE_MD].mode = D_MMOD_BACKWARD;
     g_md_h[ARM_MOVE_MD].duty = fakean * MD_GAIN;
     return EXIT_SUCCESS;
   }
-  
+
   g_md_h[ARM_MOVE_MD].duty = 0;
   g_md_h[ARM_MOVE_MD].mode = D_MMOD_FREE;
   return EXIT_SUCCESS;
@@ -125,13 +127,13 @@ static
 int suspensionSystem(void){
   const int num_of_motor = 2;/*モータの個数*/
   const int analog_max = 13;
-  int rc_analogdata;	/*コントローラから送られるアナログデータを格納*/
-  int target;		/*目標となる制御値*/
-  int gain;		/*アナログデータと掛け合わせて使うgain値*/
-  unsigned int idx;	/*インデックス*/
-  const int incr = 500;	/*Duty上昇時の変化量*/
-  const int decr = 500;	/*Duty下降時の変化量*/
-  int i;		/*カウンタ用*/
+  int rc_analogdata;    /*コントローラから送られるアナログデータを格納*/
+  int target;           /*目標となる制御値*/
+  int gain;             /*アナログデータと掛け合わせて使うgain値*/
+  unsigned int idx;     /*インデックス*/
+  const int incr = 500; /*Duty上昇時の変化量*/
+  const int decr = 500; /*Duty下降時の変化量*/
+  int i;                /*カウンタ用*/
 
   /*for each motor*/
   for( i = 0; i < num_of_motor; i++ ){
@@ -140,46 +142,51 @@ int suspensionSystem(void){
     /*それぞれの差分*/
     switch( i ){
     case DRIVE_MD_R:
-      rc_analogdata = -(DD_RCGetRY(g_rc_data));
-      if ( (__RC_ISPRESSED_R2(g_rc_data)) &&
-	  !(__RC_ISPRESSED_L2(g_rc_data)) )
-	rc_analogdata = -analog_max;
-      
-      if ( (__RC_ISPRESSED_L2(g_rc_data)) &&
-	  !(__RC_ISPRESSED_R2(g_rc_data)) )
-	rc_analogdata = analog_max;
-      
-      if (_IS_REVERSE_R)
-	rc_analogdata = -rc_analogdata;
+      rc_analogdata = -( DD_RCGetRY(g_rc_data));
+      if(( __RC_ISPRESSED_R2(g_rc_data)) &&
+         !( __RC_ISPRESSED_L2(g_rc_data))){
+        rc_analogdata = -analog_max;
+      }
+
+      if(( __RC_ISPRESSED_L2(g_rc_data)) &&
+         !( __RC_ISPRESSED_R2(g_rc_data))){
+        rc_analogdata = analog_max;
+      }
+
+      if( _IS_REVERSE_R ){
+        rc_analogdata = -rc_analogdata;
+      }
       break;
-     
+
     case DRIVE_MD_L:
-      rc_analogdata = -(DD_RCGetRY(g_rc_data));
-      if ( (__RC_ISPRESSED_R2(g_rc_data)) &&
-	  !(__RC_ISPRESSED_L2(g_rc_data)) )
-	rc_analogdata = analog_max;
-      
-      if ( (__RC_ISPRESSED_L2(g_rc_data)) &&
-	  !(__RC_ISPRESSED_R2(g_rc_data)) )
-	rc_analogdata = -analog_max;
-      
-      if (_IS_REVERSE_L)
-	rc_analogdata = -rc_analogdata;
+      rc_analogdata = -( DD_RCGetRY(g_rc_data));
+      if(( __RC_ISPRESSED_R2(g_rc_data)) &&
+         !( __RC_ISPRESSED_L2(g_rc_data))){
+        rc_analogdata = analog_max;
+      }
+
+      if(( __RC_ISPRESSED_L2(g_rc_data)) &&
+         !( __RC_ISPRESSED_R2(g_rc_data))){
+        rc_analogdata = -analog_max;
+      }
+
+      if( _IS_REVERSE_L ){
+        rc_analogdata = -rc_analogdata;
+      }
       break;
-      
+
     default:
-       message("err", "real MDs are fewer than defined");
+      message("err", "real MDs are fewer than defined");
       return EXIT_FAILURE;
-    }
-    
+    } /* switch */
+
     /*これは中央か?±3程度余裕を持つ必要がある。*/
-    if( abs(rc_analogdata) > CENTRAL_THRESHOLD ) {
+    if( abs(rc_analogdata) > CENTRAL_THRESHOLD ){
       target = rc_analogdata * gain;
-    }
-    else { 
+    }else  {
       target = 0;
     }
-    TrapezoidCtrl(target, &(g_md_h[idx]), incr, decr);
+    TrapezoidCtrl(target, &( g_md_h[idx] ), incr, decr);
   }
   return EXIT_SUCCESS;
 } /* suspensionSystem */
--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -9,23 +9,19 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
   { 0x10, /* address 駆動(Right)*/
     0, /* default duty */
     D_MMOD_FREE, /* mode */
-  },
-  { 0x11, 	/*駆動(Left)*/
-    0, 
-    D_MMOD_FREE, 
-  },
-  { 0x12,	/*ステア用EC付(Right)*/
+},
+  { 0x11,       /*駆動(Left)*/
     0,
-    D_MMOD_FREE,
-  },
-  { 0x13,	/*ステア用EC付(Left)*/
+    D_MMOD_FREE, },
+  { 0x12,       /*ステア用EC付(Right)*/
     0,
-    D_MMOD_FREE,
-  },
-  { 0x14,	/*アーム上下**/
+    D_MMOD_FREE, },
+  { 0x13,       /*ステア用EC付(Left)*/
     0,
-    D_MMOD_FREE,
-  },
+    D_MMOD_FREE, },
+  { 0x14,       /*アーム上下**/
+    0,
+    D_MMOD_FREE, },
 };
 #endif
 #if DD_NUM_OF_AB
@@ -33,9 +29,8 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
 DD_ABHand_t g_ab_h[DD_NUM_OF_AB] = {
   { 0x20, /* address (シリンダ)*/
     0x00, /* data */
-  },
-  { 0x30,	/*真空モータ*/
-    0x00,
-  },
+},
+  { 0x30,       /*真空モータ*/
+    0x00, },
 };
 #endif
