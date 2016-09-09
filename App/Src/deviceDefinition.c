4a7b67f343a21433b1f479436cc491af15a7b5d1
okapy142 <okapy142@gmail.com>
2016-09-09 12:03:55 +0900

    構造体の代入のスタイルを修正

--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -6,32 +6,32 @@
 #if DD_NUM_OF_MD
 /*MD Definition*/
 DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
-  { 0x10, /* address (駆動Right)*/
-    0, /* default duty */
-    D_MMOD_FREE, /* mode */
+  { .add = 0x10, /* address (駆動Right)*/
+    .duty = 0, /* default duty */
+    .mode = D_MMOD_FREE, /* mode */
   },
-  { 0x11, /*駆動L*/
-    0, 
-    D_MMOD_FREE, 
+  { .add = 0x11, /*駆動L*/
+    .duty = 0, 
+    .mode = D_MMOD_FREE, 
   },
-  { 0x12,	/*回転機構*/
-    0,
-    D_MMOD_FREE,
+  { .add = 0x12,	/*回転機構*/
+    .duty = 0,
+    .mode = D_MMOD_FREE,
   },
-  { 0x13,	/*リール機構*/
-    0,
-    D_MMOD_FREE,
+  { .add = 0x13,	/*リール機構*/
+    .duty = 0,
+    .mode = D_MMOD_FREE,
   },
 };
 #endif
 #if DD_NUM_OF_AB
 /*AB Definition*/
 DD_ABHand_t g_ab_h[DD_NUM_OF_AB] = {
-  { 0x20, /* address (シリンダ)*/
-    0x00, /* data */
+  { .add = 0x20, /* address (シリンダ)*/
+    .dat = 0x00, /* data */
   },
-  { 0x30,	/*真空モータ*/
-    0x00,
+  { .add = 0x30,	/*真空モータ*/
+    .dat = 0x00,
   },
 };
 #endif
@@ -43,3 +43,8 @@ DD_SV_t g_sv_h = {
   }
 };
 #endif
+
+
+
+
+
